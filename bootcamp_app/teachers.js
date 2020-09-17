0384176bff24af4ab48e17cf62b7430e2929d1c0
Audrey Cooper <audreycooper@Audreys-MacBook-Pro.local>
2020-09-16 20:16:21 -0700

    add teachers. js with databse connection and queries

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,29 @@
+
+// database connection through node postgres
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.connect()
+
+
+const cohortName = process.argv[2]
+pool.query(`
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM teachers
+  JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name LIKE '%${cohortName}%'
+  ORDER By teachers.name;
+  `)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+});
